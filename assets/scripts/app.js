a9af4dd6267d06cabb6ec6d7884a38afde5a1d57
t-kowalewski <wkowalewski@outlook.com>
2021-03-31 12:54:34 +0200

    add setSwitchHandler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,9 @@
 class Tooltip {}
 
 class Project {
-  constructor(id) {
+  constructor(id, updateProjectListFunct) {
     this.id = id;
+    this.updateProjectList = updateProjectListFunct;
 
     this.moreInfoBtn();
     this.switchBtn();
@@ -15,25 +16,32 @@ class Project {
   switchBtn() {
     const projectItemElement = document.getElementById(this.id);
     const btn = projectItemElement.querySelector('button:last-of-type');
-    btn.addEventListener('click');
+    btn.addEventListener('click', this.updateProjectList);
   }
 }
 
 class ProjectList {
   projects = [];
 
-  constructor(type, switchHandlerFunction) {
+  constructor(type) {
     this.type = type;
-    this.switchHandler = switchHandlerFunction;
 
     const listItems = document.querySelectorAll(`#${type} li`);
 
     for (const listItem of listItems) {
-      this.projects.push(new Project(listItem.id));
+      this.projects.push(
+        new Project(listItem.id, this.switchProject.bind(this))
+      );
     }
   }
 
-  addProject() {}
+  setSwitchHandler(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction;
+  }
+
+  addProject() {
+    console.log(this);
+  }
 
   switchProject(projectId) {
     this.switchHandler(this.projects.find((p) => p.d === projectId));
@@ -45,6 +53,13 @@ class App {
   static init() {
     const activeProjects = new ProjectList('active-projects');
     const finishedProjects = new ProjectList('finished-projects');
+
+    activeProjects.setSwitchHandler(
+      finishedProjects.addProject.bind(finishedProjects)
+    );
+    finishedProjects.setSwitchHandler(
+      activeProjects.addProject.bind(activeProjects)
+    );
   }
 }
 
